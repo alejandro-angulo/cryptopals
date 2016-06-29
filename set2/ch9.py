06a5be487ade62bf9ce5d8b7d7a4a7a19caf40fb
Alejandro Angulo <aab.j13@gmail.com>
2016-06-29 16:34:36 -0700

    Fixed PCKS7 padding when len(s) > blk_sz

--- set2/ch9.py
+++ set2/ch9.py
@@ -1,8 +1,8 @@
 import binascii
 import sys
 
-def pad_PCKS7(s, length):
-    pad_length = length - len(s)
+def pad_PCKS7(s, blk_sz):
+    pad_length = blk_sz - (len(s) % blk_sz)
     for i in range(pad_length):
         s += bytes( [pad_length] )
 
@@ -10,6 +10,7 @@ def pad_PCKS7(s, length):
 
 def main():
     string = b'YELLOW SUBMARINE'
+    string = b'\x895\xbe\xec\r\x85\xca\x96\xa2`test\x8aW\xbf[\x0c'
     padded_string = pad_PCKS7(string, 20)
     print(padded_string)
 
