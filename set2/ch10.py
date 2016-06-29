12da1bc599c9c24776c153e80aaa209a708e798f
Alejandro Angulo <aab.j13@gmail.com>
2016-06-29 15:42:35 -0700

    completed challenge 10

--- set2/ch10.py
+++ set2/ch10.py
@@ -1,5 +1,5 @@
 import binascii
-import itertools
+from Crypto.Cipher import AES
 
 class CBC:
     def __init__(self, ECB, IV):
@@ -7,10 +7,61 @@ class CBC:
         self._IV = IV
         self._blocksize = 16
 
+    def _partition_blocks(self, s):
+        return [s[i : i+self._blocksize] for i in range (0, len(s), self._blocksize)]
+
+    def encrypt(self, plaintext):
+        plainblocks = self._partition_blocks(plaintext)
+        prev = self._IV
+        ciphertext = b''
+      
+        for plainblock in plainblocks:
+            xortext = b''
+            for i in range( len(prev) ):
+                xortext += bytes([ plainblock[i] ^ prev[i] ])
+
+            cipherblock = self._ECB.encrypt( xortext )
+            ciphertext += cipherblock
+            prev = cipherblock
+
+        return ciphertext
+
+    def decrypt(self, ciphertext):
+        cipherblocks = self._partition_blocks(ciphertext)
+        prev = self._IV
+        plaintext = b''
+
+        for cipherblock in cipherblocks:
+            xortext = b''
+
+            plainblock = self._ECB.decrypt(cipherblock)
+            for i in range( len(prev) ):
+                xortext += bytes([ plainblock[i] ^ prev[i] ])
+
+            plaintext += xortext
+            prev = cipherblock
+
+        return plaintext
+
 def main():
+    fname = '10.txt'
+    fh = open(fname)
+    contents = binascii.a2b_base64( fh.read() )
+
     key = b'YELLOW SUBMARINE'    
     IV = bytes(16)
-    print(IV)
+
+    test = CBC( AES.new(key, AES.MODE_ECB), IV )
+    #encrypted = test.encrypt(contents)
+    #decrypted = test.decrypt(encrypted)
+    decrypted = test.decrypt(contents)
+    print(decrypted)
+    encrypted = test.encrypt(decrypted)
+
+    if encrypted == contents:
+        print('Yay')
+    else:
+        print('fuck')
 
 if __name__ == "__main__":
     main()
