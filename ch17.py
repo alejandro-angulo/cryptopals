c89f60abe4119bd8ced8933dccbe0e45b6a5ff1b
Alejandro Angulo <aab.j13@gmail.com>
2016-07-03 09:11:24 -0700

    started challenge 17

--- ch17.py
+++ ch17.py
@@ -1,3 +1,11 @@
+from Crypto.Cipher import AES
+from Crypto.Random import random
+import binascii
+import ch9
+import ch10
+import ch11
+import ch15
+
 strings = [
     b'MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=',
     b'MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=',
@@ -11,8 +19,56 @@ strings = [
     b'MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93'
 ]
 
+key = ch11.rand_bytes(16)
+iv = ch11.rand_bytes(16)
+
+def enc_oracle():
+    global key
+    global iv
+
+    cipher = ch10.CBC(AES.new(key, AES.MODE_ECB), iv)
+
+    plaintext = binascii.a2b_base64( random.choice(strings) )
+    padded = ch9.pad_PCKS7(plaintext, 16)
+
+    return cipher.encrypt(padded)
+
+def decrypt_check_pad(ciphertext):
+    global key
+    global iv
+
+    cipher = ch10.CBC(AES.new(key, AES.MODE_ECB), iv)
+    plaintext = cipher.decrypt(ciphertext)
+
+    return ch15.isValid_PKCS7(plaintext)
+
+def break_block(cipherblock, blk_sz):
+    plaintext = b''
+    for i in range(1, blk_sz + 1):
+        fake_ciphertext = bytes(blk_sz - i)
+        for j in range(256):
+            ret = decrypt_check_pad(fake_ciphertext + bytes([j] * i) + cipherblock)
+            if ret:
+                intermediate = j ^ i
+                #print(cipherblock[i - 1] ^ intermediate)
+                plaintext = bytes([cipherblock[blk_sz - 1] ^ intermediate]) + plaintext
+                break
+
+    return plaintext
+
+def break_oracle(ciphertext, blk_sz):
+    plaintext = b''
+    blocks = [ciphertext[i:i+blk_sz] for i in range(0, len(ciphertext), blk_sz)]
+
+    for j in range( len(blocks)-1, 1, -1 ):
+        plaintext = break_block(blocks[j], blk_sz) + plaintext
+
+    return plaintext
+
 def main():
-    print('test')
+    enc = enc_oracle()
+    print(enc)
+    print( break_oracle(enc, 16) )
 
 if __name__ == '__main__':
-    main()`
+    main()
